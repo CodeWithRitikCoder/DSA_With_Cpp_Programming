9663c9c04d7d4f12fa304765df7408d22a9a9a72
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2023-12-07 01:11:40 +0530

    I have added something in Introduction_To_AVL_Data_Structure.cpp file of this DSA Course.

--- Mine DSA Programs/Introduction_To_AVL_Tree_Data_Structure.cpp
+++ Mine DSA Programs/Introduction_To_AVL_Tree_Data_Structure.cpp
@@ -7,7 +7,25 @@ using namespace std;
 Notes:-
     => AVL Tree Data Structure.
 
-        -> 
+        ->What is AVL Tree?
+            -> Height balanced binary search trees.
+            -> Height difference between heights of the left and right subtrees for every nodes is less than or equal to 1.
+            -> Balanced factor= Height of right subtree - Height of left subtree.
+            -> Balance factor Can be -1, 0 or 1 for a node to be balanced in a Binary search tree.
+            -> Balance factor can be -1, 0 or 1 for all nodes of an AVL tree.
+
+        -> Why do we need an AVL Tree?
+            -> Almost all the operations in a binary search tree are of order O(h) where h is the height of the tree.
+            -> If we don't plan our tree properly, this height can get as high as n where n is the number of nodes in a BST(skewed tree).
+            -> To guarantee and upper bound of O(log n) for all these operations, we use balanced tree.
+
+
+        => Rotation in AVL Trees
+            
+            -> LL rotations in an AVL Tree
+            -> RR rotations in an AVL Tree
+            -> LR rotations in an AVL Tree
+            -> RL rotations in an AVL Tree
         
 
 */
